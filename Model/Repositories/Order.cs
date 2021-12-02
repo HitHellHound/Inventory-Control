47b3d1a280291d00962d0f03c0182d32da643c31
Ignat Churilo <i.ignatch.com@mail.ru>
2021-12-02 20:37:23 +0300

    Implement DAO

---
+++ Model/Repositories/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseAccountingSystem
+{
+    public class Order
+    {
+        public long Id { get; set; }
+        public string NameProduct { get; set; }
+        public int CostProduct { get; set; }
+        public int IdProduct { get; set; }
+        public string PaymentProduct { get; set; }
+    }
+}
